459437906b4cea2224079f19a5b3965d8fd25def
777777miSSU7777777 <6x7miSSU7x7@gmail.com>
2019-07-31 15:37:40 +0300

    ref user service entry point

--- cmd/userservice/user_service.go
+++ cmd/userservice/user_service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"net/http"
 	"os"
 
@@ -16,28 +15,16 @@ import (
 	"github.com/777777miSSU7777777/gaming-website/pkg/services/userservice"
 )
 
-var (
-	host   string
-	port   string
-	dbhost string
-	dbuser string
-	dbpass string
-	dbname string
-)
-
-func init() {
-	flag.StringVar(&host, "host", "0.0.0.0", "Defines host ip")
-	flag.StringVar(&host, "h", "0.0.0.0", "Defines host ip")
-	flag.StringVar(&port, "port", "8080", "Defines host port")
-	flag.StringVar(&port, "p", "8080", "Defines host port")
-	flag.StringVar(&dbhost, "db_host", "mysql", "Defines db host")
-	flag.StringVar(&dbuser, "db_user", "root", "Defines db user")
-	flag.StringVar(&dbpass, "db_pass", "", "Defines db user's password")
-	flag.StringVar(&dbname, "db_name", "GAMING_WEBSITE", "Defines db name")
-	flag.Parse()
-}
-
 func main() {
+	var (
+		listenAddr       string
+		connectionString string
+	)
+
+	flag.StringVar(&listenAddr, "listen_addr", ":8080", "Defines listen address")
+	flag.StringVar(&connectionString, "connection_string", "", "Defines connection string for MySQL")
+	flag.Parse()
+
 	logger := log.New()
 	jsonFormatter := &log.JSONFormatter{}
 	jsonFormatter.TimestampFormat = "2006-01-02 15:04:05"
@@ -45,8 +32,7 @@ func main() {
 	logger.SetReportCaller(true)
 	logger.SetOutput(os.Stdout)
 
-	conString := fmt.Sprintf("%s:%s@/%s", dbuser, dbpass, dbname)
-	db, err := sql.Open(dbhost, conString)
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		logger.Fatalln(err)
 	}
@@ -55,11 +41,10 @@ func main() {
 	userRepo := userrepository.New(db)
 	userSvc := userservice.WrapLoggingMiddleware(userservice.New(userRepo), logger)
 	ctx := context.Background()
-	addr := fmt.Sprintf("%s:%s", host, port)
 
 	handler := userapi.NewHttpServer(ctx, userSvc, logger)
-	logger.Infof("Server started on %s", addr)
-	err = http.ListenAndServe(addr, handler)
+	logger.Infof("Server started on %s", listenAddr)
+	err = http.ListenAndServe(listenAddr, handler)
 	if err != nil {
 		logger.Fatalln(err)
 	}
