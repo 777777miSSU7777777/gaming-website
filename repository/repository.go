972e4aa229aa120bb29625ec1fc7da50ad0f46dc
777777miSSU7777777 <6x7miSSU7x7@gmail.com>
2019-08-06 14:50:38 +0300

    add functions to repository for working with tournaments table

--- repository/repository.go
+++ repository/repository.go
@@ -89,3 +89,46 @@ func (r Repository) AddUserBalanceByID(ctx context.Context, id int64, points int
 	}
 	return nil
 }
+
+func (r Repository) NewTournament(ctx context.Context, name string, deposit int64) (int64, error) {
+	result, err := r.db.Exec("INSERT INTO TOURNAMENTS(TOURNAMENT_NAME, DEPOSIT) VALUES(?,?)", name, deposit)
+	if err != nil {
+		return -1, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return -1, err
+	}
+
+	return id, nil
+}
+
+func (r Repository) GetTournamentByID(ctx context.Context, id int64) (model.Tournament, error) {
+	row := r.db.QueryRow("SELECT * FROM TOURNAMENTS WHERE ID=?", id)
+	tournament := model.Tournament{}
+	err := row.Scan(&tournament.ID, &tournament.TournamentName, &tournament.Deposit, &tournament.Prize)
+	if err != nil {
+		return model.Tournament{}, err
+	}
+
+	return tournament, nil
+}
+
+func (r Repository) AddUserToTournament(ctx context.Context, tournamentID int64, userID int64) error {
+	_, err := r.db.Exec("INSERT INTO MTM_USER_TOURNAMENT(TOURNAMENT_ID, USER_ID) VALUES (?,?)", tournamentID, userID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r Repository) DeleteTournamentByID(ctx context.Context, id int64) error {
+	_, err := r.db.Exec("DELETE FROM TOURNAMENTS WHERE ID=?", id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
