ae88e964c7400376f64974c04571688af4e98d89
777777miSSU7777777 <6x7miSSU7x7@gmail.com>
2019-08-16 18:09:20 +0300

    fix docker push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-IMAGE=$DOCKER_LOGIN:$IMAGE_NAME
-
-docker build -t $IMAGE:$TAG .
+docker build -t $IMAGE_NAME:$TAG .
 
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_LOGIN --password-stdin
 
-docker tag $IMAGE:$TAG $DOCKER_LOGIN/$IMAGE:$TAG
+docker tag $IMAGE_NAME:$TAG $DOCKER_LOGIN/$IMAGE_NAME:$TAG
 
-docker push $DOCKER_LOGIN/$IMAGE:$TAG
+docker push $DOCKER_LOGIN/$IMAGE_NAME:$TAG
