69c281176c403da73959caac9e2ee32eda0c95d8
777777miSSU7777777 <6x7miSSU7x7@gmail.com>
2019-08-19 11:51:51 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ ssh -tt -i blabla.pem $SSH_USER@$SERVER_IP <<EOF
         cd $LOCAL_REPO
     else 
         cd $LOCAL_REPO
+        git checkout $TRAVIS_BRANCH
         git pull $REPO_SRC
     fi
 
-    git checkout deploy
     docker-compose stop
-    echo "TAG=$(git rev-parse HEAD)" > .env
+    echo "$TAG" > .env
     docker-compose up -d
     exit
 EOF
